148ee8db6e814eb0d22c0e23cd55d67343ce55e1
alk2pb <altk01@gmail.com>
2014-12-18 23:46:45 -0500

    aesthetic updates

--- default/static/js/script.js
+++ default/static/js/script.js
@@ -1,14 +1,23 @@
 $(document).ready(function() {
-  
   // Make more-btns visible
   var dimension = 0;
-  $(".btn-fab-mini").each(function() {
+  $(".btn-fab-more").each(function() {
     dimension = dimension + 72;
+  });
+  
+  if ($(window).height() < dimension){
+    $(".btn-fab-more").each(function() {
+      $(this).attr("data-placement","top");
+    });
+  }
+  
+  $(".footer").each(function() {
+    $(this).width($(this).parent().width());
   });
   
   if ($(window).width() >= 767){
     $("#more-btn-group").mouseenter(function() {
-      $(".btn-fab-mini").each(function() {
+      $(".btn-fab-more").each(function() {
         $(this).removeClass("under");
         
       });
@@ -16,15 +25,15 @@ $(document).ready(function() {
       
       if ($(window).height() >= dimension){
         $("#more-btn-group").css("height", dimension);
-        $(".animation-rotate").rotate({duration:200,animateTo:540})
+        $(".animation-rotate").rotate({duration:200,animateTo:180})
       }
       else {
         $("#more-btn-group").css("width", dimension);
-        $(".animation-rotate").rotate({duration:200,animateTo:450})
+        $(".animation-rotate").rotate({duration:200,animateTo:90})
       }
     });
     $("#more-btn-group").mouseleave(function() {
-      $(".btn-fab-mini").each(function() {
+      $(".btn-fab-more").each(function() {
         $(this).addClass("under");
       });
       $("#more-btn-group").removeClass("in");
@@ -42,22 +51,22 @@ $(document).ready(function() {
   else {
     $("html").click(function(event) {
       if (jQuery.contains(document.getElementById("more-btn-group"),event.target) && !$("#more-btn-group").hasClass("in")){
-        $(".btn-fab-mini").each(function() {
+        $(".btn-fab-more").each(function() {
           $(this).removeClass("under");
         });
         $("#more-btn-group").addClass("in");
         
         if ($(window).height() >= dimension){
           $("#more-btn-group").css("height", dimension);
-          $(".animation-rotate").rotate({duration:200,animateTo:540});
+          $(".animation-rotate").rotate({duration:200,animateTo:180});
         }
         else {
           $("#more-btn-group").css("width", dimension);
-          $(".animation-rotate").rotate({duration:200,animateTo:450});
+          $(".animation-rotate").rotate({duration:200,animateTo:90});
         }
       }
-      else {
-        $(".btn-fab-mini").each(function() {
+      else if ($("#more-btn-group").hasClass("in")) {
+        $(".btn-fab-more").each(function() {
           $(this).addClass("under");
         });
         $("#more-btn-group").removeClass("in");
@@ -101,11 +110,28 @@ $(document).ready(function() {
           timeout = true;
           setTimeout(resizeend, delta);
       }
-      if ($(window).height() >= dimension){
-        $(".animation-rotate").rotate({duration:200,animateTo:0})
+      
+      if ($(window).height() >= dimension) {
+        if ($("#more-btn-group").hasClass("in")) {
+          $(".animation-rotate").rotate({duration:200,animateTo:180})
+        }
+        else {
+          $(".animation-rotate").rotate({duration:200,animateTo:0})
+        }
       }
       else {
-        $(".animation-rotate").rotate({duration:200,animateTo:-90})
+        if ($("#more-btn-group").hasClass("in")) {
+          $(".animation-rotate").rotate({duration:200,animateTo:90})
+        }
+        else {
+          $(".animation-rotate").rotate({duration:200,animateTo:-90})
+        }
+      }
+      
+      if ($(window).height() < dimension) {
+        $(".btn-fab-more").each(function() {
+          $(this).attr("data-placement","top");
+        });
       }
   });
   
@@ -156,12 +182,12 @@ $(document).ready(function() {
   
   // Adjust heights of intro and contact sections
   
-  var height = $(window).height();
+  var height = $(window).height() - 1;
   if ($("#intro").height() < height){
     $("#intro").height(height);
   }
-  if ($("#contact").height() < height){
-    $("#contact").height(height);
+  if ($("#contact").height() + 200 < height){
+      $("#contact").height(height - 200);
   }
   
   //jQuery to collapse the navbar on scroll
