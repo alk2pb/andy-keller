c31e53fcb2ddec3c8207629950964020749a1ab7
alk2pb <altk01@gmail.com>
2014-12-22 03:59:21 -0500

    update

--- default/static/js/script.js
+++ default/static/js/script.js
@@ -4,25 +4,25 @@ $(document).ready(function() {
   $(".btn-fab-more").each(function() {
     dimension = dimension + 72;
   });
-  
+
   if ($(window).height() < dimension){
     $(".btn-fab-more").each(function() {
       $(this).attr("data-placement","top");
     });
   }
-  
+
   $(".footer").each(function() {
     $(this).width($(this).parent().width());
   });
-  
+
   if ($(window).width() >= 767){
     $("#more-btn-group").mouseenter(function() {
       $(".btn-fab-more").each(function() {
         $(this).removeClass("under");
-        
+
       });
       $("#more-btn-group").addClass("in");
-      
+
       if ($(window).height() >= dimension){
         $("#more-btn-group").css("height", dimension);
         $(".animation-rotate").rotate({duration:200,animateTo:180})
@@ -37,7 +37,7 @@ $(document).ready(function() {
         $(this).addClass("under");
       });
       $("#more-btn-group").removeClass("in");
-      
+
       if ($(window).height() >= dimension){
         $("#more-btn-group").css("height", 76);
         $(".animation-rotate").rotate({duration:200,animateTo:0})
@@ -55,7 +55,7 @@ $(document).ready(function() {
           $(this).removeClass("under");
         });
         $("#more-btn-group").addClass("in");
-        
+
         if ($(window).height() >= dimension){
           $("#more-btn-group").css("height", dimension);
           $(".animation-rotate").rotate({duration:200,animateTo:180});
@@ -70,7 +70,7 @@ $(document).ready(function() {
           $(this).addClass("under");
         });
         $("#more-btn-group").removeClass("in");
-        
+
         if ($(window).height() >= dimension){
           $("#more-btn-group").css("height", 76);
           $(".animation-rotate").rotate({duration:200,animateTo:0});
@@ -82,10 +82,10 @@ $(document).ready(function() {
       }
     });
   }
-  
+
   // Position the profile-picture-collapse and resume-link elements correctly
   var position_profile = $(".navbar-brand-name").position();
-  
+
   if ($(window).width() >= 767){
     $(".profile-picture-collapse").css("left",position_profile.left + 145);
   }
@@ -93,14 +93,14 @@ $(document).ready(function() {
     //$(".profile-picture-collapse").css("left",position_profile.left + 135);
     $(".profile-picture-collapse").css("left",$(".navbar").width()/2 - 25);
   }
-  
+
   var position_resume = $("#resume").find(".section-heading").position();
   var resumepositionleft = position_resume.left + $("#resume").find(".section-heading").width() - 26;
   var resumepositiontop = position_resume.top + 10;
-  
+
   $(".resume-link").css("left",resumepositionleft);
   $(".resume-link").css("top",resumepositiontop);
-  
+
   var rtime = new Date(1, 1, 2000, 12,00,00);
   var timeout = false;
   var delta = 200;
@@ -110,7 +110,7 @@ $(document).ready(function() {
           timeout = true;
           setTimeout(resizeend, delta);
       }
-      
+
       if ($(window).height() >= dimension) {
         if ($("#more-btn-group").hasClass("in")) {
           $(".animation-rotate").rotate({duration:200,animateTo:180})
@@ -127,20 +127,20 @@ $(document).ready(function() {
           $(".animation-rotate").rotate({duration:200,animateTo:-90})
         }
       }
-      
+
       if ($(window).height() < dimension) {
         $(".btn-fab-more").each(function() {
           $(this).attr("data-placement","top");
         });
       }
   });
-  
+
   function resizeend() {
       if (new Date() - rtime < delta) {
           setTimeout(resizeend, delta);
       } else {
           timeout = false;
-          
+
           position_profile = $(".navbar-brand-name").position();
           if ($(window).width() >= 767){
             $(".profile-picture-collapse").css("left",position_profile.left + 145);
@@ -149,18 +149,18 @@ $(document).ready(function() {
             //$(".profile-picture-collapse").css("left",position_profile.left + 135);
             $(".profile-picture-collapse").css("left",$(".navbar").width()/2 - 25);
           }
-          
+
           position_resume = $("#resume").find(".section-heading").position();
-      
+
           var resumepositionleft = position_resume.left + $("#resume").find(".section-heading").width() - 26;
-          
+
           var resumepositiontop = position_resume.top + 10;
-          
+
           $(".resume-link").css("left",resumepositionleft);
           $(".resume-link").css("top",resumepositiontop);
       }
   }
-  
+
   // Prevent collapse event from being triggered unless navbar-toggle is clicked
   // or navbar-collapse is visible and the click event is off of the navbar
   $("html").click(function(event) {
@@ -170,7 +170,7 @@ $(document).ready(function() {
       }
     }
   });
-  
+
   // Change shadow of the profile-picture with mouseover
   $(".navbar-brand").mouseover(function() {
     if ($(window).width() >= 767){
@@ -180,17 +180,17 @@ $(document).ready(function() {
   $(".navbar-header").mouseout(function() {
     $(".profile-picture-collapse").removeClass("shadow-z-2-hover");
   });
-  
-  // Adjust heights of intro and contact sections
-  
+
+  // Adjust heights of intro and connect sections
+
   var height = $(window).height();
   if ($("#intro").height() < height){
     $("#intro").height(height);
   }
-  if ($("#contact").height() + 200 < height){
-      $("#contact").height(height - 201);
+  if ($("#connect").height() + 200 < height){
+      $("#connect").height(height - 201);
   }
-  
+
   //jQuery to collapse the navbar on scroll
   $(window).scroll(function() {
       if ($(".navbar").offset().top > 50) {
@@ -205,7 +205,7 @@ $(document).ready(function() {
           $(".profile-picture-collapse").fadeOut(800);
       }
   });
-  
+
   //jQuery for page scrolling feature - requires jQuery Easing plugin
   $(function() {
       $('.page-scroll a').bind('click', function(event) {
